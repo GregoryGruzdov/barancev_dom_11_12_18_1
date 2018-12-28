4208095279491557ec748592e586543714c2bfd5
Turqu <norfnorflotr@gmail.com>
2018-12-28 16:05:15 +0300

    наполовину поменял файл для пайтона. ЕЩЕ не удалил класс

--- addressbook2.py
+++ addressbook2.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 from selenium.webdriver.firefox.webdriver import WebDriver
 import unittest
+import pytest
 from group import Group
+from application import Application
 
 def is_alert_present(wd):
     try:
@@ -10,62 +12,28 @@ def is_alert_present(wd):
     except:
         return False
 
+@pytest.fixture
+def app(request):
+    fixture = Application()
+    request.addfinalizer(fixture.destroy)
+    return fixture
+
 class addressbook2(unittest.TestCase):
     def setUp(self):
-        self.wd = WebDriver()
-        self.wd.implicitly_wait(60)
-
-    def open_home_page(self):
-        wd = self.wd
-        wd.get("http://localhost/addressbook/")
-
-    def login(self, username, password):
-        wd = self.wd
-        self.open_home_page() 
-        wd.find_element_by_name("user").click()
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys(username)
-        wd.find_element_by_name("pass").click()
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys(password)
-        wd.find_element_by_css_selector("input[type=\"submit\"]").click()
-
-    def group_creation(self, group):
-        wd = self.wd
-        # open groups page
-        wd.find_element_by_link_text("groups").click()
-        # init group creation
-        wd.find_element_by_name("new").click()
-        # fill group form
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys(group.groupname)
-        wd.find_element_by_name("group_header").click()
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(group.groupheader)
-        wd.find_element_by_name("group_footer").click()
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(group.groupfooter)
-        # submit group creation
-        wd.find_element_by_name("submit").click()
-        wd.find_element_by_link_text("group page").click()
-
-    def logout(self):
-        wd = self.wd
-        wd.find_element_by_link_text("Logout").click()
+        self.app = Application()
 
     def test_add_group(self):
-        self.login(username="admin", password="secret")
-        self.group_creation(Group(groupname="123", groupheader="123", groupfooter="123"))
-        self.logout()
+        self.app.login(username="admin", password="secret")
+        self.app.group_creation(Group(groupname="123", groupheader="123", groupfooter="123"))
+        self.app.logout()
 
     def test_add_empty_group(self):
-        self.login(username="admin", password="secret")
-        self.group_creation(Group(groupname="", groupheader="", groupfooter=""))
-        self.logout()
+        self.app.login(username="admin", password="secret")
+        self.app.group_creation(Group(groupname="", groupheader="", groupfooter=""))
+        self.app.logout()
 
     def tearDown(self):
-        self.wd.quit()
+        self.app.destroy()
 
 if __name__ == '__main__':
     unittest.main()
