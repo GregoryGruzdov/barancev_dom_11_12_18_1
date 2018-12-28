55dca487692d2e9655e70fb5b7fccb1b059ee67e
Turqu <norfnorflotr@gmail.com>
2018-12-28 16:19:55 +0300

    полностью перевел код на pytest

--- addressbook2.py
+++ addressbook2.py
@@ -1,16 +1,7 @@
 # -*- coding: utf-8 -*-
-from selenium.webdriver.firefox.webdriver import WebDriver
-import unittest
 import pytest
 from group import Group
 from application import Application
-
-def is_alert_present(wd):
-    try:
-        wd.switch_to_alert().text
-        return True
-    except:
-        return False
 
 @pytest.fixture
 def app(request):
@@ -18,22 +9,12 @@ def app(request):
     request.addfinalizer(fixture.destroy)
     return fixture
 
-class addressbook2(unittest.TestCase):
-    def setUp(self):
-        self.app = Application()
+def test_add_group(app):
+    app.login(username="admin", password="secret")
+    app.group_creation(Group(groupname="123", groupheader="123", groupfooter="123"))
+    app.logout()
 
-    def test_add_group(self):
-        self.app.login(username="admin", password="secret")
-        self.app.group_creation(Group(groupname="123", groupheader="123", groupfooter="123"))
-        self.app.logout()
-
-    def test_add_empty_group(self):
-        self.app.login(username="admin", password="secret")
-        self.app.group_creation(Group(groupname="", groupheader="", groupfooter=""))
-        self.app.logout()
-
-    def tearDown(self):
-        self.app.destroy()
-
-if __name__ == '__main__':
-    unittest.main()
+def test_add_empty_group(app):
+    app.login(username="admin", password="secret")
+    app.group_creation(Group(groupname="", groupheader="", groupfooter=""))
+    n  app.logout()
