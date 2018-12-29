56ac73914d5c3946ca47f23c5bea8898a3d525a4
Turqu <norfnorflotr@gmail.com>
2018-12-29 13:19:20 +0300

    добавил класс помощник, SessionHelper, перенес туда login logout, подключил к нему WD через главный класс application,

--- fixture/application.py
+++ fixture/application.py
@@ -1,9 +1,11 @@
 from selenium.webdriver.firefox.webdriver import WebDriver
+from fixture.session import SessionHelper
 
 class Application:
     def __init__(self):
         self.wd = WebDriver()
         self.wd.implicitly_wait(60)
+        self.session = SessionHelper(self)
 
     def open_home_page(self):
         wd = self.wd
--- fixture/session.py
+++ fixture/session.py
@@ -1,8 +1,11 @@
 class SessionHelper:
 
+    def __init__(self,app):
+        self.app = app
+
     def login(self, username, password):
-        wd = self.wd
-        self.open_home_page()
+        wd = self.app.wd
+        self.app.open_home_page()
         wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
         wd.find_element_by_name("user").send_keys(username)
@@ -12,5 +15,5 @@ class SessionHelper:
         wd.find_element_by_css_selector("input[type=\"submit\"]").click()
 
     def logout(self):
-        wd = self.wd
+        wd = self.app.wd
         wd.find_element_by_link_text("Logout").click()
