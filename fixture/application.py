ba2570c2a7783dc4b23f384681825ca8ef44eda8
Turqu <norfnorflotr@gmail.com>
2018-12-29 13:52:19 +0300

    добавил вспомогательный класс, еше не тестил

--- fixture/application.py
+++ fixture/application.py
@@ -1,35 +1,18 @@
 from selenium.webdriver.firefox.webdriver import WebDriver
 from fixture.session import SessionHelper
 
+
 class Application:
+
     def __init__(self):
         self.wd = WebDriver()
         self.wd.implicitly_wait(60)
         self.session = SessionHelper(self)
+        self.group = GroupHelper(self)
 
     def open_home_page(self):
         wd = self.wd
         wd.get("http://localhost/addressbook/")
-
-    def group_creation(self, group):
-        wd = self.wd
-        # open groups page
-        wd.find_element_by_link_text("groups").click()
-        # init group creation
-        wd.find_element_by_name("new").click()
-        # fill group form
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys(group.groupname)
-        wd.find_element_by_name("group_header").click()
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(group.groupheader)
-        wd.find_element_by_name("group_footer").click()
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(group.groupfooter)
-        # submit group creation
-        wd.find_element_by_name("submit").click()
-        wd.find_element_by_link_text("group page").click()
 
     def destroy(self):
         self.wd.quit()
