9b930e3f991abf6ce8eba9075590e5df4dc96965
Turqu <norfnorflotr@gmail.com>
2019-01-05 14:18:28 +0300

    сделал тест, модель, вспомогательный класс

--- fixture/group.py
+++ fixture/group.py
@@ -3,10 +3,19 @@ class GroupHelper:
     def __init__(self, app):
         self.app = app
 
+    def open_groups_page(self):
+        wd = self.app.wd
+        wd.find_element_by_link_text("groups").click()
+
+    def return_to_groups_page(self):
+        wd = self.app.wd
+        wd.find_element_by_link_text("group page").click()
+
+
     def create(self, group):
         wd = self.app.wd
         # open groups page
-        wd.find_element_by_link_text("groups").click()
+        self.open_groups_page()
         # init group creation
         wd.find_element_by_name("new").click()
         # fill group form
@@ -22,4 +31,15 @@ class GroupHelper:
         # submit group creation
         wd.find_element_by_name("submit").click()
         # go back to groups page
-        wd.find_element_by_link_text("group page").click()
+        self.return_to_groups_page()
+
+    def delete_first_group(self):
+        wd = self.app.wd
+        # open groups page
+        self.open_groups_page()
+        # select first group
+        wd.find_element_by_name("selected[]").click()
+        # submit deletion
+        wd.find_element_by_name("delete").click()
+        # return to groups page
+        self.return_to_groups_page()
