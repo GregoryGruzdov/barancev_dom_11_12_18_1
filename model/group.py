471c120f4ccd09af3cb327cfbb7cf7c14db8236f
Turqu <norfnorflotr@gmail.com>
2019-01-07 22:55:43 +0300

    добавил новый тест на модификацию, использовал монструкцию if then else во вспомогательном методе,

--- model/group.py
+++ model/group.py
@@ -1,5 +1,5 @@
 class Group:
-    def __init__(self, groupheader, groupname, groupfooter):
+    def __init__(self, groupheader=None, groupname=None, groupfooter=None):
         self.groupheader = groupheader
         self.groupname = groupname
         self.groupfooter = groupfooter 
--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -1,13 +1,14 @@
 from model.group import Group
 
+
 def test_modify_group_name(app):
     app.session.login(username="admin", password="secret")
-    app.group.modify_first_group_name(Group(groupname="SISKA"))
+    app.group.modify_first_group(Group(groupname="name"))
     app.session.logout()
 
 
 def test_modify_group_header(app):
     app.session.login(username="admin", password="secret")
-    app.group.modify_first_group_header(Group(groupheader="SISKA"))
+    app.group.modify_first_group(Group(groupheader="header"))
     app.session.logout()
 
