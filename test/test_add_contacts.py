a209396f0aaec355a45e7bd12bc1160f9b846fe3
Turqu <norfnorflotr@gmail.com>
2019-01-08 17:27:28 +0300

    вынес логин и логаут в метод, вызывающий фикстуру. теперь они одни на всю сессию

--- test/test_add_contacts.py
+++ test/test_add_contacts.py
@@ -3,6 +3,4 @@
 from model.contact import Contact
 
 def test_add_contact(app):
-    app.session.login(username="admin", password="secret")
     app.contact.create(Contact(firstname="123", lastname="123", middlename="123", address2="123", email="email.email@email.com"))
-    app.session.logout()
--- test/test_add_group.py
+++ test/test_add_group.py
@@ -4,13 +4,9 @@ from model.group import Group
 
 
 def test_add_group(app):
-    app.session.login(username="admin", password="secret")
-    app.group.create(Group(groupname="123", groupheader="123", groupfooter="123"))
-    app.session.logout()
+    app.group.create(Group(groupname="123", ))
 
 
 def test_add_empty_group(app):
-    app.session.login(username="admin", password="secret")
     app.group.create(Group(groupname="", groupheader="", groupfooter=""))
-    app.session.logout()
 
